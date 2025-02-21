07fafbace10875bd8f2e5c9b2e0b4c0c39f46d20
agungbhaskara23 <agungbhaskara35@gmail.com>
2025-02-21 15:07:20 +0700

    Add files via upload

---
+++ daftar-belanja/app/components/GroceryList.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { useState } from "react";
+import Item from "./Item";
+
+export default function GroceryList({items, onDeleteItem, onToggleItem, onClearItems={onClearItems}}){
+    const [sortBy, setSortBy] = useState('input');
+    
+    let sortedItems;
+  
+    switch(sortBy){
+      case 'name':
+        sortedItems = sortedItems = items.slice().sort((a,b) => a.name.localeCompare(b.name));
+        break;
+      case 'checked':
+        sortedItems = sortedItems = items.slice().sort((a,b) => a.checked - b.checked);
+        break;
+      default:
+        sortedItems = items;
+        break;
+    }
+  
+    return(
+      <>
+      <div className="list">
+        <ul>
+          {
+            sortedItems.map((item) => (
+              <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
+            ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button onClick={onClearItems}>Bersihkan Daftar</button>
+      </div>
+      </>
+    )
+  }
